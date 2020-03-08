0202b1d7692cdfe0a85144b6b0b021d2a369ceb1
soma_dev_magome <soma.dev.magome@gmail.com>
2020-03-08 21:23:14 +0900

    no message

--- hm-client/src/main.js
+++ hm-client/src/main.js
@@ -3,9 +3,9 @@ import App from "./App.vue";
 import router from "@/router/router.js";
 import store from "./store";
 
-import ElementUI from "element-ui"; // 追記
-import locale from "element-ui/lib/locale/lang/ja"; // 追記
-import "element-ui/lib/theme-chalk/index.css"; // 追記
+import ElementUI from "element-ui";
+import locale from "element-ui/lib/locale/lang/ja";
+import "element-ui/lib/theme-chalk/index.css"; 
 
 Vue.config.devtools = true;
 
--- hm-client/src/router/router.js
+++ hm-client/src/router/router.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue';
 import Router from "vue-router";
 import Home from "@/views/Home.vue";
 
 Vue.use(Router);
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
