84061a4ff162ab56994aeb782d5edb5dadf37fde
soma_dev_magome <soma.dev.magome@gmail.com>
2020-03-22 19:58:25 +0900

    no message

--- spring-vue/src/main/java/hm/springapi/domain/Account.java
+++ spring-vue/src/main/java/hm/springapi/domain/Account.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -43,4 +45,10 @@ public class Account {
     @Column
     @Temporal(TemporalType.TIMESTAMP)
     private Date deleted_at;
+    
+    /****/
+    @ManyToOne
+    @JoinColumn(name="account_type_id",referencedColumnName="id",nullable=false,unique=true,insertable = false, updatable = false)
+    private AccountType accountType;
+    
 }
