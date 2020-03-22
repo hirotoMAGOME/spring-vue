e4c2d53a0855a2b0468d515cf2308a13eecab708
soma_dev_magome <soma.dev.magome@gmail.com>
2020-03-22 23:13:28 +0900

    no message

--- spring-vue/src/main/resources/db/migration/V3__create_table_user.sql
+++ spring-vue/src/main/resources/db/migration/V3__create_table_user.sql
@@ -1,15 +1,21 @@
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS m_user;
 
 -- user
-CREATE TABLE `user` (
+CREATE TABLE `m_user` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(64) NOT NULL,
   `password` VARCHAR(64) NOT NULL,
-  `admin_kbn` VARCHAR(10) NOT NULL,
-  `del_flg` BOOLEAN NOT NULL DEFAULT 0,
+  `user_kbn` VARCHAR(2) NOT NULL DEFAULT "02",
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_user_id` bigint unsigned NOT NULL DEFAULT 0,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_user_id` bigint unsigned NOT NULL DEFAULT 0,
+  `deleted_at` DATETIME,
+  `deleted_user_id` bigint unsigned DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO user VALUES (NULL,'Soma', 'Hiroto12', '1',0);
-INSERT INTO user VALUES (NULL,'test', 'test', '0',0);
+INSERT INTO m_user (id,name,password,user_kbn) VALUES (1,'admin', 'admin', '01');
+INSERT INTO m_user (name,password,user_kbn) VALUES ('soma', 'soma', '01');
+INSERT INTO m_user (name,password,user_kbn) VALUES ('test', 'test', '01');
