fc77833608f6cb470f016abc8d36d09bf819bb18
soma_dev_magome <soma.dev.magome@gmail.com>
2020-05-03 03:03:16 +0900

    success insert

--- spring-vue/src/main/java/hm/springapi/controller/CashController.java
+++ spring-vue/src/main/java/hm/springapi/controller/CashController.java
@@ -3,6 +3,7 @@ package hm.springapi.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class CashController {
     private final CashService cashService;
 
     @GetMapping("/cash")
+    @CrossOrigin
     public ResponseEntity<CashResponse> findAll() {
     	//ëΩï™ÉfÅ[É^ÇÇ∆Ç¡ÇƒÇ´ÇƒÇÈ
     	List<Budget> budgets = cashService.findAll();
@@ -28,12 +30,11 @@ public class CashController {
                 .budgets(budgets)
                 .build();
         return new ResponseEntity<>(cashResponse, HttpStatus.OK);
-//    	String json = "{'currencies':[{'id':10001,'name':'Bitcoin','symbol':'BTC','amount':0E-20},{'id':10002,'name':'Ethereum','symbol':'ETH','amount':0E-20}]}";
-//    	return new ResponseEntity<>(json, HttpStatus.OK);
     }
     
     @PostMapping("/cash")
+    @CrossOrigin
     public String messagePost() {
-    	return "messages";
+    	return cashService.createActual();
     }
 }
--- spring-vue/src/main/java/hm/springapi/service/CashService.java
+++ spring-vue/src/main/java/hm/springapi/service/CashService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import hm.springapi.domain.Budget;
 import hm.springapi.domain.BudgetRepository;
+import hm.springapi.domain.Actual;
+import hm.springapi.domain.ActualRepository;
 
 import java.util.List;
 
@@ -15,5 +17,22 @@ public class CashService {
 
     public List<Budget> findAll() {
         return budgetRepository.findAll();
+    }
+    
+    private final ActualRepository actualRepository;
+    
+    public String createActual() {
+    	Actual actual = new Actual();
+    	actual.setBudgetId(1);
+    	actual.setAccountId(1);
+    	actual.setPrice(999);
+    	actual.setName("test");
+//    	actual.setCreatedAt(java.sql.Date.valueOf("2020-01-01 00:00:00"));
+//    	actual.setCreatedUserId(1);
+//    	actual.setUpdatedAt(java.sql.Date.valueOf("2020-01-01 00:00:00"));
+    	
+    	actualRepository.save(actual);
+    	
+    	return "success!!";
     }
 }
