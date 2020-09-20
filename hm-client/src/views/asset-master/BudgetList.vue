394cdc2f3fcb2b782f670cdf2ac84232c2c78abd
soma_dev_magome <soma.dev.magome@gmail.com>
2020-09-21 00:25:38 +0900

    no message

--- hm-client/src/views/asset-master/BudgetList.vue
+++ hm-client/src/views/asset-master/BudgetList.vue
@@ -1,7 +1,7 @@
 <template>
   <el-card class="box-card" shadow="always" body-style="padding:10px 20px">
     <div slot="header" class="clearfix">
-      <span>予算カテゴリ一覧</span>
+      <span>予算一覧</span>
     </div>
     <el-row>
       <el-button
@@ -11,11 +11,21 @@
           onClickEdit(0)
           dialogFormVisible = true
         "
-      >追加</el-button>
+      >追加</el-button>予算カテゴリ：
+      <el-select filterable placeholder="Select" v-model="selectedBudgetCategory">
+        <el-option
+          v-for="item in options.budgetCategories"
+          :key="item.id"
+          :label="item.name"
+          :value="item.id"
+        >{{ item.name }}</el-option>
+      </el-select>
     </el-row>
-    <el-table :data="options.budgetCategories" border style="width: 100%">
+    <el-table :data="options.budgets" border style="width: 100%">
       <el-table-column prop="id" label="ID" width="180"></el-table-column>
-      <el-table-column prop="name" label="予算カテゴリ名" width="180"></el-table-column>
+      <el-table-column prop="budgetCategoryId" label="予算カテゴリ名" width="180"></el-table-column>
+      <el-table-column prop="name" label="予算名" width="180"></el-table-column>
+      <el-table-column prop="amount" label="予算" width="180"></el-table-column>
       <el-table-column label="編集" width="180">
         <template slot-scope="scope">
           <el-button
@@ -44,10 +54,24 @@
       <el-form :model="form">
         <el-form ref="form" :model="form" label-width="200px" size="medium">
           <el-form-item label="ID">{{ form.id }}</el-form-item>
-          <el-form-item label="予算カテゴリ名">
+          <el-form-item label="予算カテゴリ">
+            <el-select filterable placeholder="Select" v-model="form.budgetCategoryId">
+              <el-option
+                v-for="item in options.budgetCategories"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              >{{ item.name }}</el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="予算名">
             <el-input v-model="form.name"></el-input>
           </el-form-item>
-          <el-form-item label="固定費/変動費">
+          <el-form-item label="予算">
+            <el-input v-model="form.amount"></el-input>
+          </el-form-item>
+
+          <!-- <el-form-item label="固定費/変動費">
             <el-radio-group v-model="form.budgetCategoryType">
               <el-radio
                 v-for="v in options.budgetCategoryType"
@@ -55,7 +79,7 @@
                 :label="v.clsTypeKey"
               >{{v.name}}</el-radio>
             </el-radio-group>
-          </el-form-item>
+          </el-form-item>-->
         </el-form>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -77,23 +101,53 @@
 import axios from "axios"
 import common from "@/js/common/common.js"
 
-const API_PATH_AST_01 = "http://localhost:8080/api/ast/budget-category"
+const API_PATH_AST_02 = "http://localhost:8080/api/ast/budget"
 
 export default {
-  name: "BudgetCategoryList",
+  name: "BudgetList",
   mixins: [common],
   data() {
     return {
       options: {
-        budgetCategories: [],
-        budgetCategoryType: []
+        budgets: [],
+        budgetCategories: [
+          // TODO APIで取得
+          {
+            id: 1,
+            name: "予算カテゴリ１"
+          },
+          {
+            id: 2,
+            name: "予算カテゴリ２"
+          },
+          {
+            id: 3,
+            name: "予算カテゴリ３"
+          },
+          {
+            id: 4,
+            name: "予算カテゴリ４"
+          },
+          {
+            id: 5,
+            name: "予算カテゴリ５"
+          },
+          {
+            id: 6,
+            name: "予算カテゴリ６"
+          }
+        ]
       },
       dialogFormVisible: false, //モーダルの表示状態
       form: {
         id: null,
+        budgetCategoryId: null,
         name: null,
-        budgetCategoryType: false
-      }
+        amount: null
+        // selectedBudgetCategory: null
+        // budgetCategoryType: false
+      },
+      selectedBudgetCategory: null
     }
   },
   created: function() {
@@ -102,12 +156,12 @@ export default {
     that.getFromApi()
 
     //GET(種別)
-    that
-      .getClsType("budget_category_type", true)
-      .then(that.setClsType, that.setClsTypeErr)
+    // that
+    //   .getClsType("budget_category_type", true)
+    //   .then(that.setClsType, that.setClsTypeErr)
 
     //初期値をセット
-    that.display()
+    that.display(that)
   },
   mounted: function() {},
   methods: {
@@ -127,43 +181,51 @@ export default {
 
       //GETの実行
       axios
-        .get(API_PATH_AST_01)
+        .get(API_PATH_AST_02)
         .then(function(res) {
-          that.options.budgetCategories = res.data.budgetCategories
+          that.options.budgets = res.data.budgets
         })
         .catch(function(err) {
           console.log("ERROR")
           console.log(err)
         })
     },
-    setClsType: function(data) {
-      var that = this
-      that.options.budgetCategoryType = data.budgetCategoryType
+    // setClsType: function(data) {
+    //   var that = this
+    //   that.options.budgetCategoryType = data.budgetCategoryType
+    // },
+    // setClsTypeErr: function(data) {
+    //   console.log(data)
+    // },
+    display: function(that) {
+      that.options.budgetCategories.push({
+        id: 0,
+        name: "指定なし"
+      })
+      // debugger
+      that.options.budgetCategories.sort(that.objectArraySort("id", "asc"))
+      // debugger
     },
-    setClsTypeErr: function(data) {
-      console.log(data)
-    },
-    display: function() {},
     onClickEdit: function(selectedId) {
       //モーダルに値をセット
       if (selectedId === 0) {
         this.form = {
           id: null,
+          budgetCategoryId: null,
           name: null,
-          budgetCategoryType: null
+          amount: null
         }
       } else {
-        var getData = this.options.budgetCategories.find(
-          v => v.id === selectedId
-        )
+        var getData = this.options.budgets.find(v => v.id === selectedId)
 
         this.form = {
           id: getData.id,
+          budgetCategoryId: getData.budgetCategoryId,
           name: getData.name,
-          budgetCategoryType: getData.budgetCategoryType
+          amount: getData.amount
         }
       }
-
+      console.log(this.form)
       //モーダルを開く
       // this.dialogFormVisible = true;
     },
@@ -177,7 +239,7 @@ export default {
       }
 
       axios
-        .patch(API_PATH_AST_01, request)
+        .patch(API_PATH_AST_02, request)
         .then(function(response) {
           console.log("ok")
           console.log(response)
@@ -192,7 +254,7 @@ export default {
       // var params = { id: selectedId }
       //DELETEの実行
       axios
-        .delete(API_PATH_AST_01 + "/" + selectedId)
+        .delete(API_PATH_AST_02 + "/" + selectedId)
         .then(function(response) {
           console.log("ok")
           console.log(response)
